5c063054504591cd214502eec97b60f80338b678
Jamie Burns <jamieb28@gmail.com>
2013-05-17 17:49:48 -0400

    second commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+	def index
+		@name = "Jamie"
+	end
+	def about
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Blog::Application.routes.draw do
+  resources :posts
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+
+  match '/about' => 'welcome#about'  
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -48,11 +53,13 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
+
 end
