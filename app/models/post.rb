fff056f2e6a5d45046d7685fa7c96d49912f744c
Jamie Burns <jamieb28@gmail.com>
2013-05-24 13:01:38 -0400

    third commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
+  	attr_accessible :content, :name, :title
 
-  validates :name,  :presence => true
-  validates :title, :presence => true
-  validates :content, :presence => true,
+  	validates :name,  :presence => true
+  	validates :title, :presence => true
+  	validates :content, :presence => true,
                     :length => { :minimum => 5 }
 
+	has_many :comments, :dependent => :destroy
 end
